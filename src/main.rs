f4f3852b66f7cfc7b443d1543c3f5636827f7ab5
Fa1tinthesky <fate.public@gmail.com>
2025-03-25 14:26:34 +0500

    Added error handling for if port is in use.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::{ 
-    fs, 
+    fs, process,
     io::{prelude::*, BufReader},
-    net::{SocketAddr, TcpListener, TcpStream}
+    net::{SocketAddr, TcpListener, TcpStream},
+    thread::{sleep},
+    time::Duration
 };
 
 
 fn main() {
     let aux_addrs = generate_aux_ports(8080);
-    let listener = match TcpListener::bind(&aux_addrs[..]) {
-        Ok(val) => { println!("The server is running on {:?}", val.local_addr().unwrap()); val },
-        Err(err) => panic!("Error occured!!!")
-    };
+    let fk_addrs = vec![SocketAddr::from(([127, 0, 0 ,1], 8070))]; // For testing purposes.
+
+    let listener = start_ws(aux_addrs).unwrap_or_else(|| {
+        println!("FAILED TO START A SERVER. TIMEOUT ERROR");
+        process::exit(1);
+    });
 
     for stream in listener.incoming() {
         println!("Buyaaaah, connection was made~!!!!");
@@ -23,6 +27,34 @@ fn main() {
     
 }
 
+
+fn start_ws(addrs: Vec<SocketAddr>) -> Option<TcpListener> {
+    let max_attempts = 5;
+    let mut attempts = 0;
+
+    let listener = loop {
+        match TcpListener::bind(&addrs[..]) {
+            Ok(listener) => { 
+                println!("Server is running on: {:?}", listener.local_addr().unwrap());
+                break Some(listener);
+            },
+            Err(err) => {
+                attempts += 1;
+                if attempts > max_attempts {
+                    return None;
+                }
+
+                println!("{}/{} COULDN'T START A SERVER. RETRYING...", attempts, max_attempts);
+
+
+                sleep(Duration::new(3, 0));
+            }
+        }
+    };
+
+    return listener;
+}
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
@@ -33,9 +65,6 @@ fn handle_connection(mut stream: TcpStream) {
 
     // println!("Request: {http_request:#?}");
 
-    //let mut file = File::open("./html/index.html").unwrap();
-    //let mut contents = String::new();
-    //file.read_to_string(&mut contents).unwrap();
     let status_line = "HTTP/1.1 200 OK";
     let contents = fs::read_to_string("./html/index.html").unwrap();
     let length = contents.len();
